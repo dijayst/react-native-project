589959dc4e15cc8c035c548fb8a531082b5eb382
esther olubiyi <olubiyisther@gmail.com>
2025-06-21 01:47:35 +0100

    camerasetup

--- App.js
+++ App.js
@@ -1,14 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
 import VideoRecordingScreen from './src/components/VideoRecordingScreen';
 
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { TouchableOpacity } from "react-native";
+//import Notes from "./components/Notes/NotesScreen";
+//import AddNote from "./components/Notes/AddNote";
+//import ViewNote from "./components/Notes/ViewNote";
+//import VideoScreen from './components/Camera/Video';
+
+
+
+
+function HomeScreen({ navigation }) {  
+  return (
+   <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+     <TouchableOpacity
+       style={{
+         width: "80%",
+         padding: 5,
+         borderColor: "red",
+         borderWidth: 1,
+         borderRadius: 15,
+         marginVertical: 10
+       }}
+     >
+       <Text
+         style={{ textAlign: "center", color: "red" }}
+         onPress={() =>
+           navigation.navigate("NotesScreen")
+         }
+       >
+         Notes
+       </Text>
+     </TouchableOpacity>
+     <TouchableOpacity
+       style={{
+         width: "80%",
+         padding: 5,
+         borderColor: "red",
+         borderWidth: 1,
+         borderRadius: 15,
+         marginVertical: 10
+       }}
+     >
+       <Text
+         style={{ textAlign: "center", color: "red" }}
+         onPress={() =>
+           navigation.navigate("VideoRecordingScreen")
+         }
+       >
+         Camera
+       </Text>
+     </TouchableOpacity>
+     <TouchableOpacity
+       style={{
+         width: "80%",
+         padding: 5,
+         borderColor: "red",
+         borderWidth: 1,
+         borderRadius: 15,
+         marginVertical: 10
+       }}
+     >
+       <Text
+         style={{ textAlign: "center", color: "red" }}
+         onPress={() =>
+           navigation.navigate("Video")
+         }
+       >
+         Video
+       </Text>
+     </TouchableOpacity>
+   </View>
+ );
+}
+
+
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <VideoRecordingScreen/>
-    </View>
+    <NavigationContainer>
+     <Stack.Navigator>
+       <Stack.Screen name="Home" component={HomeScreen} />
+       <Stack.Screen name="VideoRecordingScreen" component={VideoRecordingScreen}/>
+     </Stack.Navigator>
+   </NavigationContainer>
   );
 }
 
--- src/components/VideoRecordingScreen.js
+++ src/components/VideoRecordingScreen.js
@@ -1,22 +1,126 @@
-import { View, Text ,StyleSheet} from 'react-native'
-import React from 'react'
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { useNavigation } from "@react-navigation/native";
+import * as MediaLibrary from "expo-media-library";
+import Slider from "@react-native-community/slider";
+
 
 export default function VideoRecordingScreen() {
+
+
+
+    const [cameraPermission, setCameraPermission] = useState(); //State variable for camera permission
+  const [mediaLibraryPermission, setMediaLibraryPermission] = useState(); //State variable for media library permission
+  const [micPermission, setMicPermission] = useState(); //// state variable for microphone permission
+  const [cameraMode, setCameraMode] = useState("picture"); //State variable for picture or video. By default it will be for picture
+  const [facing, setFacing] = useState("back");
+  const [photo, setPhoto] = useState(); //After picture is taken this state will be updated with the picture
+  const [video, setVideo] = useState(); //After video is recorded this state will be updated
+  const [flashMode, setFlashMode] = useState("on"); //Camera Flash will be ON by default
+  const [recording, setRecording] = useState(false); //State will be true when the camera will be recording
+  const [zoom, setZoom] = useState(0); //State to control the digital zoom
+  let cameraRef = useRef(); //Creates a ref object and assigns it to the variable cameraRef.
+  const navigation = useNavigation();
+
+
+
+ useEffect(() => {
+    (async () => {
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      const mediaLibraryPermission =
+        await MediaLibrary.requestPermissionsAsync();
+      const microphonePermission =
+        await Camera.requestMicrophonePermissionsAsync();
+      setCameraPermission(cameraPermission.status === "granted");
+      setMediaLibraryPermission(mediaLibraryPermission.status === "granted");
+      setMicPermission(microphonePermission.status === "granted");
+    })();
+  }, []);
+
+  //If permissions are not granted app will have to wait for permissions
+  if (
+    cameraPermission === undefined ||
+    mediaLibraryPermission === undefined ||
+    micPermission === undefined
+  ) {
+    return <Text>Request Permissions....</Text>;
+  } else if (!cameraPermission) {
+    return (
+      <Text>
+        Permission for camera not granted. Please change this in settings
+      </Text>
+    );
+  }
+
+  //Function to toggle between back and front camera
+  function toggleCameraFacing() {
+    setFacing((current) => (current === "back" ? "front" : "back"));
+  }
+
+
+
+  
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>VideoRecordingScreen</Text>
+      <CameraView style={styles.camera} facing={facing}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </CameraView>
     </View>
-  )
+  );
 }
-
-
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
